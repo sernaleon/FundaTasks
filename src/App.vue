b5cc780f43dbbc0bc1b1e044f487a8a97be03d3d
Arturo <arturo@funda.nl>
2021-02-20 11:34:51 +0100

    Add base url per environment

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div>{{ tasks }}</div>
 </template>
 
 <script>
+import axios from 'axios'
+import configuration from './configuration'
+
 export default {
   name: "App",
   data() {
     return {
-      value: "World"
+      tasks: "loading...",
     };
+  },
+  created: async function() {
+    const response = await axios.get(`${configuration.apiBaseUrl}/api/tasks`);
+    this.tasks = response.data.text;
   }
 };
 </script>
