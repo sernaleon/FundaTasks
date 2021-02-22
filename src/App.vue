7b11bfd0afd086911786e936ba19cdc7ac1b76dc
Arturo Serna Leon <Arturo@funda.nl>
2021-02-22 12:57:06 +0100

    Update template

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>{{ tasks }}</div>
+  <div>
+    <ul id="example-1">
+      <li v-for="item in tasks" :key="item.id">
+        {{ item.task.name }}
+        {{ item.description }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-import configuration from './configuration'
+import axios from "axios";
+import configuration from "./configuration";
 
 export default {
   name: "App",
   data() {
     return {
-      tasks: "loading...",
+      tasks: undefined,
     };
   },
-  created: async function() {
+  created: async function () {
     const response = await axios.get(`${configuration.apiBaseUrl}/api/tasks`);
-    this.tasks = response.data.text;
-  }
+    this.tasks = response.data;
+  },
 };
 </script>
