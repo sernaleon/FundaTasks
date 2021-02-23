c7fee2d863202094b7a0701bdc99f0807746380f
Arturo Serna Leon <Arturo@funda.nl>
2021-02-23 23:38:01 +0100

    Change theme to be more funda-alike

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-});
+    theme: {
+      themes: {
+        light: {
+          primary: colors.orange.darken1,
+          secondary: colors.orange.lighten4,
+          accent: colors.indigo.base,
+        },
+      },
+    },
+  });
